2e92023168d55068e34b370203e5b1db7acb9948
kang ming <kangmingpc@gmail.com>
2024-04-05 14:21:53 +0800

    update: updated parse tree, tree node to store lexeme

--- include/parseTree.h
+++ include/parseTree.h
@@ -3,18 +3,21 @@
 #include <stddef.h>
 #include "lexer.h"
 
-typedef struct TreeNode {
+typedef struct TreeNode
+{
     char *label;
-    struct TreeNode *parent; 
+    struct TreeNode *parent;
     struct TreeNode **children;
     size_t num_children;
+    char *lexeme;
 } TreeNode;
 
-
-TreeNode *createNode(char *label);
+TreeNode *createNode(char *label, char *lexeme);
 void addChild(TreeNode *parent, TreeNode *child);
 void printParseTree(TreeNode *root, int depth);
 TreeNode *buildParseTree(Token *tokens, size_t num_tokens);
 TreeNode *buildParseTreeFromTokens(Token **tokens, size_t num_tokens);
+
+PythonTokenType peekToken(Token *token);
 
 #endif
--- src/parseTree.c
+++ src/parseTree.c
@@ -4,77 +4,113 @@
 #include <stdlib.h>
 #include <string.h>
 
-
-TreeNode *createNode(char *label) {
+TreeNode *createNode(char *label, char *lexeme)
+{
     TreeNode *node = (TreeNode *)malloc(sizeof(TreeNode));
     node->label = strdup(label);
     node->parent = NULL;
     node->children = NULL;
     node->num_children = 0;
+    node->lexeme = strdup(lexeme);
     return node;
 }
 
-void addChild(TreeNode *parent, TreeNode *child) {
+void addChild(TreeNode *parent, TreeNode *child)
+{
     parent->children = (TreeNode **)realloc(parent->children, (parent->num_children + 1) * sizeof(TreeNode *));
     parent->children[parent->num_children] = child;
     parent->num_children++;
-    child->parent = parent; 
+    child->parent = parent;
 }
 
-void printParseTree(TreeNode *root, int depth) {
+void printParseTree(TreeNode *root, int depth)
+{
     size_t i;
-    for (i = 0; i < depth; i++) {
+    for (i = 0; i < depth; i++)
+    {
         printf("  ");
     }
     printf("%s\n", root->label);
-    for (i = 0; i < root->num_children; i++) {
+    for (i = 0; i < root->num_children; i++)
+    {
         printParseTree(root->children[i], depth + 1);
     }
 }
 
-TreeNode *buildParseTreeFromTokens(Token **tokens, size_t num_tokens) {
-    TreeNode *program = createNode("Program");
+PythonTokenType peekToken(Token *token)
+{
+    if (token != NULL)
+    {
+        return token->type;
+    }
+    else
+    {
+        return UNKNOWN;
+    }
+}
+
+TreeNode *buildParseTreeFromTokens(Token **tokens, size_t num_tokens)
+{
+    TreeNode *program = createNode("Program", "Program");
     TreeNode *current_node = program;
-    size_t i=0;
+    TreeNode *base_node = NULL;
+    size_t i = 0;
     printf("\nbuildparsetree start\n");
-    while (i < num_tokens) {
+    while (i < num_tokens)
+    {
         Token *current_token = tokens[i];
-        printf("%d\n",current_token->type);
-        switch (current_token->type) {
-            case PYTOK_DEF:
-                addChild(current_node, createNode("FunctionDefinition"));
-                current_node = current_node->children[current_node->num_children - 1];
-                break;
-            case PYTOK_LEFTPARENTHESIS:
-                addChild(current_node, createNode("ParameterList"));
-                current_node = current_node->children[current_node->num_children - 1];
-                break;
-            case PYTOK_IDENTIFIER:
-                addChild(current_node, createNode("Identifier"));
-                current_node = current_node->children[current_node->num_children - 1];
-                break;
-            case PYTOK_RIGHTPARENTHESIS:
+        PythonTokenType nextTokenType = peekToken(tokens[i + 1]);
+        printf("%d\n", current_token->type);
+        switch (current_token->type)
+        {
+        case PYTOK_DEF:
+            addChild(current_node, createNode("FunctionDefinition", current_token->lexeme));
+            base_node = current_node;
+            current_node = current_node->children[current_node->num_children - 1];
+            break;
+        case PYTOK_LEFTPARENTHESIS:
+            addChild(current_node, createNode("ParameterList", current_token->lexeme));
+            current_node = current_node->children[current_node->num_children - 1];
+            break;
+        case PYTOK_IDENTIFIER:
+            addChild(current_node, createNode("Identifier", current_token->lexeme));
+            current_node = current_node->children[current_node->num_children - 1];
+            break;
+        case PYTOK_RIGHTPARENTHESIS:
+            if (nextTokenType == PYTOK_DEF)
+            {
+                current_node = base_node;
+            }
+            else
+            {
                 current_node = current_node->parent;
-                break;
-            case PYTOK_COLON:
-                current_node = current_node->parent;
-                break;
-            case PYTOK_PRINT:
-                addChild(current_node, createNode("PrintStatement"));
-                current_node = current_node->children[current_node->num_children - 1];
-                break;
-            case PYTOK_STRING:
-                addChild(current_node, createNode("StringLiteral"));
-                current_node = current_node->children[current_node->num_children - 1];
-                break;
-            default:
-                break;
+            }
+            break;
+        case PYTOK_COLON:
+            current_node = current_node->parent;
+            break;
+        case PYTOK_PRINT:
+            addChild(current_node, createNode("PrintStatement", current_token->lexeme));
+            current_node = current_node->children[current_node->num_children - 1];
+            break;
+        case PYTOK_STRING:
+            addChild(current_node, createNode("StringLiteral", current_token->lexeme));
+            current_node = current_node->children[current_node->num_children - 1];
+            break;
+        case PYTOK_FOR:
+            addChild(current_node, createNode("ForStatement", current_token->lexeme));
+            current_node = current_node->children[current_node->num_children - 1];
+            break;
+        case PYTOK_IN:
+            addChild(current_node, createNode("InKeyword", current_token->lexeme));
+            current_node = current_node->children[current_node->num_children - 1];
+            break;
+        default:
+            break;
         }
         i++;
     }
-    
+
     printf("\nbuildparsetree end\n");
     return program;
 }
-
-
