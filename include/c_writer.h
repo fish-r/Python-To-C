6cab5e3a220b70ffe22e1f8c3fade2588b067b72
fish-r <gyf8800@gmail.com>
2024-04-08 17:07:13 +0800

    feature: recursive traversal; to update state

--- include/c_writer.h
+++ include/c_writer.h
@@ -3,9 +3,7 @@
 #include "parseTree.h"
 
 typedef enum {
-  STATE_IDLE,
-  STATE_TRAVERSING,
-  STATE_DONE,
+    STATE_INIT,
   WRITE_FN_DEF,
   WRITE_IF_STMT
 } State;
@@ -14,5 +12,6 @@ void traverse_tree(TreeNode *root);
 void set_state(State * current_state, TreeNode *current_node);
 void write_c_file(TreeNode *root);
 void write_to_file(char *filename, char *content);
-
+void process_node(TreeNode *current_node, State *current_state,
+                  TreeNode **queue, int front);
 #endif
--- src/c_writer.c
+++ src/c_writer.c
@@ -13,84 +13,75 @@ void traverse_tree(TreeNode *root) {
   /* Create a stack to store the nodes */
   TreeNode *queue[MAX_NODES];
   TreeNode *current_node;
+  State current_state = STATE_INIT;
   int front = -1;
   int rear = -1;
   int i = 0;
 
-  /* Initialize the FSM */
-  State current_state = STATE_IDLE;
-
   /* Start traversing the tree */
-  while (current_state != STATE_DONE) {
-    switch (current_state) {
-    case STATE_IDLE:
-      /* Enqueue the root node */
-      queue[++rear] = root;
-      current_state = STATE_TRAVERSING;
-      break;
+  /* Check if the queue is empty AND current node has no children */
+  /* printf("Front, rear, num children %d %d %d\n", front, rear,
+         (int)(queue[front]->num_children)); */
 
-    case STATE_TRAVERSING:
-      /* Check if the queue is empty */
-      if (front == rear) {
-        current_state = STATE_DONE;
-        break;
+  /* Enqueue the root node */
+  queue[++rear] = root;
+
+  if (front == rear && queue[front]->num_children == 0) {
+    /* printf("Done \n"); */
+    return;
+  }
+
+  /* Dequeue the front node */
+  current_node = queue[++front];
+  /* Process the current node */
+  set_state(&current_state, current_node);
+  printf("Current Node: %s \n", current_node->label);
+  process_node(current_node, &current_state, queue, front);
+  /* Enqueue the children of the current node */
+  for (i = 0; i < current_node->num_children; i++) {
+    queue[++rear] = current_node->children[i];
+    traverse_tree(current_node->children[i]);
+  }
+}
+
+void process_node(TreeNode *current_node, State *current_state,
+                  TreeNode **queue, int front) {
+  printf("Current State: %d, Current Label: %s\n", *current_state,
+         current_node->label);
+  if (*current_state == WRITE_FN_DEF) {
+
+    if (strcmp(current_node->label, "FunctionDefinition") == 0) {
+      /*TODO: change to return type later*/
+      write_to_file("output.c", "void ");
+
+    } else if (strcmp(current_node->label, "Identifier") == 0) {
+      write_to_file("output.c", current_node->lexeme);
+      write_to_file("output.c", "( ");
+
+    } else if (strcmp(current_node->label, "Parameter") == 0) {
+      write_to_file("output.c", current_node->lexeme);
+      /* implement lookahead */
+      if (strcmp(queue[front + 1]->label, "Parameter") == 0) {
+        write_to_file("output.c", ", ");
       }
-      /* Dequeue the front node */
-      current_node = queue[++front];
-      /* Process the current node */
-      /*printf("Current Node: %s \n", current_node->label);*/
-      /* Enqueue the children of the current node */
-      for (i = 0; i < current_node->num_children; i++) {
-        queue[++rear] = current_node->children[i];
-      }
-      set_state(&current_state, current_node);
-      break;
+    } else if (strcmp(current_node->label, "ReturnStatement") == 0) {
+      write_to_file("output.c", "return ");
 
-    case WRITE_FN_DEF:
-      printf("Label: %s\n", current_node->label);
-      if (strcmp(current_node->label, "FunctionDefinition") == 0) {
-        /*TODO: change to return type later*/
-        write_to_file("output.c", "void ");
-      } else if (strcmp(current_node->label, "Identifier") == 0) {
-        write_to_file("output.c", current_node->lexeme);
-        write_to_file("output.c", "( ");
-      } else if (strcmp(current_node->label, "Parameter") == 0) {
-        write_to_file("output.c", current_node->lexeme);
-        /* implement lookahead */
-        if (strcmp(queue[front + 1]->label, "Parameter") == 0) {
-          write_to_file("output.c", ", ");
-        }
-      } else if (strcmp(current_node->label, "ReturnStatement") == 0) {
-        write_to_file("output.c", "return ");
-      } else if (strcmp(current_node->label, "Block") == 0) {
-        current_state = STATE_TRAVERSING;
-        write_to_file("output.c", ") {\n");
-        break;
-      }
-      current_node = queue[++front];
-
-      break;
-
-    case WRITE_IF_STMT:
-      printf("Writing If Statement\n");
-      if (strcmp(current_node->label, "Block") == 0) {
-        current_state = STATE_TRAVERSING;
-      }
-      break;
-
-    case STATE_DONE:
-      /* Do nothing */
-      printf("Done\n");
-      break;
+    } else if (strcmp(current_node->label, "Block") == 0) {
+      write_to_file("output.c", ") {\n");
+      printf("Current state: %d\n", *current_state);
     }
   }
 }
 
 void set_state(State *current_state, TreeNode *current_node) {
+  printf("Current State: %d\n", *current_state);
   if (strcmp(current_node->label, "FunctionDefinition") == 0) {
     *current_state = WRITE_FN_DEF;
   } else if (strcmp(current_node->label, "IfStatement") == 0) {
     *current_state = WRITE_IF_STMT;
+  } else {
+    return;
   }
 }
 
